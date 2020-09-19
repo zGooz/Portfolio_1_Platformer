bb0b1788583e2464cb4d60810a23cce1a4b5128d
zGooz <ya.asde-12a@yandex.ru>
2020-09-19 12:31:59 +0300

    Created a menu, a game state container, and a framework for the player.

---
+++ Assets/Code/main manager/GameProcess.cs
@@ -0,0 +1,53 @@
+﻿
+using UnityEngine;
+
+public class GameProcess : MonoBehaviour
+{
+    [SerializeField] private Canvas _canvas;
+    [SerializeField] private GameObject _menuGameStarter;
+    [SerializeField] private GameObject _menuGameResumer;
+    [SerializeField] private GameObject _meniGameEnderOrRestarter;
+
+    public GameObject GameStarter => _menuGameStarter;
+    public GameObject GameResumer => _menuGameResumer;
+    public GameObject GameEnderOrRestarter => _meniGameEnderOrRestarter;
+
+    private GameStartOrEnd _gameStarterComponent;
+
+    private const int GAME = 0;
+    private const int MENU = 1;
+    private const int PAUSE = 2;
+    private const int WINNER = 3;
+    private const int LOSE = 4;
+
+    private int GameState = MENU;
+
+    private void Awake()
+    {
+        _menuGameStarter = Instantiate(_menuGameStarter, _canvas.transform);
+        _gameStarterComponent = _menuGameStarter.GetComponent<GameStartOrEnd>();
+    }
+
+    private void OnEnable()
+    {
+        _gameStarterComponent.GameRun += OnGameRun;
+        _gameStarterComponent.GameDone += OnGameDone;
+    }
+
+    private void OnDisable()
+    {
+        _gameStarterComponent.GameRun -= OnGameRun;
+        _gameStarterComponent.GameDone -= OnGameDone;
+    }
+
+    private void OnGameRun()
+    {
+        GameState = GAME;
+        Destroy(_menuGameStarter);
+    }
+
+    private void OnGameDone()
+    {
+        Application.Quit();
+    }
+}
